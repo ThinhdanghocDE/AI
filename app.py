9ff9793f3e1c7f3a5c07da03c46b41787229d919
ThinhdanghocBE <vuthinh122004@gmail.com>
2025-03-10 02:09:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,11 @@ sepal_width = st.sidebar.slider('Sepal Width', 2.0, 4.5, 3.0)
 petal_length = st.sidebar.slider('Petal Length', 1.0, 7.0, 4.0)
 petal_width = st.sidebar.slider('Petal Width', 0.1, 2.5, 1.0)
 
+user_input = [[sepal_length, sepal_width, petal_length, petal_width]]
 # Make predictions
 prediction = clf.predict([[sepal_length, sepal_width, petal_length, petal_width]])
 
 # Display prediction
-st.write('## Prediction:')
-st.write(iris.target_names[prediction[0]])
+if st.button("Dự đoán"):
+    prediction = model.predict(user_input)
+    st.write("Kết quả dự đoán:", prediction)
